d095d07a8b7ae63e0617fb6e052022b76462b612
wonsungmoon6 <wonsungmoon6@gmail.com>
2025-04-19 17:13:30 +0900

    DATABASE, HTML 값 전달

--- python/app.py
+++ python/app.py
@@ -13,7 +13,10 @@ conn = pymysql.connect(
 
 @app.route("/")
 def hello_world():
+    return "<h1>테스트 성공!!</h1>"
 
+@app.route('/test')
+def hello():
     curs = conn.cursor()
 
     # sql = "SELECT column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = 'customers';"
@@ -25,11 +28,7 @@ def hello_world():
     for row in rows:
         print(row)
 
-    return "<h1>테스트 성공!!</h1>"
-
-@app.route('/test')
-def hello():
-    return render_template('index.html')
+    return render_template('test.html', value = row)
 
 if __name__ == "__main__":
     app.run(debug=True)
